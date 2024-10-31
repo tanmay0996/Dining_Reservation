3746d6df027867e5975fa08556745e7c7c963f57
1663Ankesh <1663ankesh@gmail.com>
2024-10-31 21:06:28 +0530

    Deploy_5.3

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:3000", // Development URL
   `${process.env.REACT_APP_Front_End}`, // Production URL
+  `${process.env.REACT_APP_Host_Api}`, // Backend Production URL
+  `${process.env.REACT_APP_Stripe_Server}`, // Stripe Service URL
 ];
 
 app.use(
