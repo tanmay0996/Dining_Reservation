63f273ad12816408daf90a3bf7097bd57bdb082c
Ankesh <141862530+1663Ankesh@users.noreply.github.com>
2024-11-01 19:18:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -142,6 +142,10 @@ app.use((req, res, next) => {
   res.status(404).json({ error_not_found: "page not found" });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
 });
