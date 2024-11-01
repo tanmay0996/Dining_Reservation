4af8bd5302f97ea866316176560bd6660f0b8147
Ankesh <141862530+1663Ankesh@users.noreply.github.com>
2024-11-01 20:12:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -138,6 +138,10 @@ app.post("/logout", async (req, res) => {
   }
 });
 
+app.get((req,res) => {
+  res.send('Server is working');
+});
+
 app.use((req, res, next) => {
   res.status(404).json({ error_not_found: "page not found" });
 });
